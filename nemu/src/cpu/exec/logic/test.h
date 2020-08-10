b2e5cade330d94d6385c4fa1236992fe39a12803
tracer-ics2016 <tracer@njuics.org>
2020-08-10 22:10:53 +0800

    >  compile NEMU 10602121 coa Linux coa-VM 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:10:53 up 10:13,  4 users,  load average: 0.18, 0.28, 0.18 935573d1f5e41efee1c4b4d8e73543642413ec74

--- nemu/src/cpu/exec/logic/test.h
+++ nemu/src/cpu/exec/logic/test.h
@@ -6,7 +6,7 @@ make_helper(test_i2rm_b);
 make_helper(test_r2rm_b);
 
 //make_helper(test_i2a_v);
-//make_helper(test_i2rm_v);
+make_helper(test_i2rm_v);
 //make_helper(test_si2rm_v);
 make_helper(test_r2rm_v);
 
