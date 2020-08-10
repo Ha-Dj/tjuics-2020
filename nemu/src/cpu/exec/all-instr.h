9b8692e349accef6dd06556839fbbf97a4024d16
tracer-ics2016 <tracer@njuics.org>
2020-08-10 12:06:00 +0800

    >  run 10602121 coa Linux coa-VM 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:06:00 up 9 min,  1 user,  load average: 0.27, 0.93, 0.64 124a23255258941584ae68c0ca5f50743ce140f0

---
+++ nemu/src/cpu/exec/all-instr.h
@@ -0,0 +1,35 @@
+#include "prefix/prefix.h"
+
+#include "data-mov/mov.h"
+#include "data-mov/xchg.h"
+#include "data-mov/push.h"
+#include "data-mov/pop.h"
+
+#include "arith/cmp.h"
+#include "arith/dec.h"
+#include "arith/inc.h"
+#include "arith/neg.h"
+#include "arith/imul.h"
+#include "arith/mul.h"
+#include "arith/idiv.h"
+#include "arith/div.h"
+
+#include "logic/and.h"
+#include "logic/or.h"
+#include "logic/not.h"
+#include "logic/xor.h"
+#include "logic/sar.h"
+#include "logic/shl.h"
+#include "logic/shr.h"
+#include "logic/shrd.h"
+#include "logic/test.h"
+
+#include "control/call.h"
+#include "control/ret.h"
+#include "control/jcc.h"
+
+#include "string/rep.h"
+
+#include "misc/misc.h"
+
+#include "special/special.h"
---
+++ nemu/src/cpu/exec/data-mov/pop.c
@@ -0,0 +1,28 @@
+#include "cpu/exec/helper.h"
+
+#define DATA_BYTE 2
+#include "pop-template.h"
+#undef DATA_BYTE
+
+#define DATA_BYTE 4
+#include "pop-template.h"
+#undef DATA_BYTE
+
+/* for instruction encoding overloading */
+
+make_helper_v(pop_r)
+
+/*make_helper(popal) {
+	cpu.eax = swaddr_read(cpu.esp + 28, 4, R_SS);
+	cpu.ecx = swaddr_read(cpu.esp + 24, 4, R_SS);
+	cpu.edx = swaddr_read(cpu.esp + 20, 4, R_SS);
+	cpu.ebx = swaddr_read(cpu.esp + 16, 4, R_SS);
+	cpu.ebp = swaddr_read(cpu.esp +  8, 4, R_SS);
+	cpu.esi = swaddr_read(cpu.esp +  4, 4, R_SS);
+	cpu.edi = swaddr_read(cpu.esp +  0, 4, R_SS);
+	cpu.esp += 32;
+
+	print_asm("popal");
+
+	return 1;
+}*/
