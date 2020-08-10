e64b57a00e0f6624ed3e9e1e645870390f5e4560
tracer-ics2016 <tracer@njuics.org>
2020-08-10 21:27:38 +0800

    >  compile NEMU 10602121 coa Linux coa-VM 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:27:38 up  9:30,  4 users,  load average: 0.75, 0.43, 0.29 e0978c9156393db73a75ced99f36f4b9d629ed0b

--- nemu/src/cpu/exec/string/movs-template.h
+++ nemu/src/cpu/exec/string/movs-template.h
@@ -3,7 +3,7 @@
 #define instr movs
 
 make_helper(concat(movs_, SUFFIX)) {
-	MEM_W(R_ES, cpu.edi, MEM_R(R_DS, cpu.esi));
+	MEM_W(cpu.edi, MEM_R(cpu.esi));
 	cpu.esi += (cpu.eflags.DF ? -DATA_BYTE : DATA_BYTE);
 	cpu.edi += (cpu.eflags.DF ? -DATA_BYTE : DATA_BYTE);
 
