3f571cfa44f114d393ceb43fe161f4b999258519
weijizeng <weijizeng@tju.edu.cn>
2020-08-11 06:36:00 +0800

    all pass

--- nemu/src/cpu/exec/logic/shr-template.h
+++ nemu/src/cpu/exec/logic/shr-template.h
@@ -11,10 +11,6 @@ static void do_execute () {
 	OPERAND_W(op_dest, dest);
         update_eflags_pf_zf_sf(dest);
 
-	/* There is no need to update EFLAGS, since no other instructions 
-	 * in PA will test the flags updated by this instruction.
-	 */
-
 	print_asm_template2();
 }
 
