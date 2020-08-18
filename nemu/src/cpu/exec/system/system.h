a3eca0b0f7b937f56d3c7650a7bebb5d6cefc3a2
nemu_tju <hittju_2004@163.com>
2020-08-18 22:15:44 +0800

    Segment Pass!

--- nemu/src/cpu/exec/system/system.h
+++ nemu/src/cpu/exec/system/system.h
@@ -2,13 +2,7 @@
 #define __SYSTEM_H__
 
 make_helper(lgdt);
-//make_helper(lidt);
 make_helper(mov_r2cr);
 make_helper(mov_cr2r);
-//make_helper(int_);
-//make_helper(cli);
-//make_helper(sti);
-//make_helper(iret);
-//make_helper(hlt);
 
 #endif
