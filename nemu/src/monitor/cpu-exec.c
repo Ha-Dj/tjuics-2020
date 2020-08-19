949146c883c8b1ebe99d7234e8fc63df5921aaf3
tracer-ics2016 <tracer@njuics.org>
2020-08-19 20:04:29 +0800

    >  compile NEMU 10602121 mips32 Linux mips32-soc 4.15.0-112-generic #113~16.04.1-Ubuntu SMP Fri Jul 10 04:37:07 UTC 2020 i686 i686 i686 GNU/Linux  20:04:29 up  5:31,  3 users,  load average: 0.29, 0.17, 0.10 a52035fcf6353895a97636ecacbc9fdca5edb1da

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -60,7 +60,7 @@ void cpu_exec(volatile uint32_t n) {
 
 		/* Execute one instruction, including instruction fetch,
 		 * instruction decode, and the actual execution. */
-                printf("enter %x\n", cpu.eip);
+                //printf("enter %x\n", cpu.eip);
 		int instr_len = exec(cpu.eip);
 
 		cpu.eip += instr_len;
@@ -73,6 +73,7 @@ void cpu_exec(volatile uint32_t n) {
 		if(n_temp < MAX_INSTR_TO_PRINT) {
 			printf("%s\n", asm_buf);
 		}
+		printf("%x, %x\n", cpu.eip, cpu.esp);
 #endif
 
 		/* TODO: check watchpoints here. */
