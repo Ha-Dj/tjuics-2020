0e43503a375f13e5a1c2c5d9e50c235ed0d78feb
tracer-ics2016 <tracer@njuics.org>
2020-08-19 22:20:11 +0800

    >  compile NEMU 10602121 mips32 Linux mips32-soc 4.15.0-112-generic #113~16.04.1-Ubuntu SMP Fri Jul 10 04:37:07 UTC 2020 i686 i686 i686 GNU/Linux  22:20:11 up  7:46,  3 users,  load average: 0.26, 0.08, 0.02 945e8bb8c55499cd523ec7d767ca6dead1462b

--- nemu/src/memory/mmu/tlb.c
+++ nemu/src/memory/mmu/tlb.c
@@ -58,7 +58,7 @@ static uint32_t tlb_fetch(TLB *t, lnaddr_t lnaddr) {
 	/* TLB fill */
 	//t->key[idx].val = (lnaddr & ~PAGE_MASK) | 0x1;
         //int i = (lnaddr & ~PAGE_MASK) | 0x1;
-        printf("+++%x\n", lnaddr); 
+        //printf("+++%x\n", lnaddr); 
 	t->pte[idx] = page_walk(lnaddr);
         printf("****%x\n", lnaddr);
 	return idx;
